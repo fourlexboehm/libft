5f1a6432ca17cea68b744763a4330e146863b5e5
Alex Boehm <alexboehm@systemli.org>
2021-09-12 11:47:27 +0930

    yeet

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: aboehm <aboehm@42adel.org>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 18:01:38 by aboehm            #+#    #+#             */
-/*   Updated: 2021/09/11 18:07:51 by aboehm           ###   ########.fr       */
+/*   Updated: 2021/09/11 21:34:25 by aboehm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,20 @@
 
 int	ft_memcmp(const void *sp1, const void *sp2, size_t n)
 {
-	int unsigned	i;
-	char			*s1;
-	char			*s2;
+	const unsigned	char *p1;
+	const unsigned	char *p2;
 
-	s1 = (char *)sp1;
-	s2 = (char *)sp2;
-	i = 0;
-	while ((s1[i] || s2[i]) && (i < n))
+	if (n != 0)
 	{
-		if (s1[i] < s2[i])
+		p1 = sp1;
+		p2 = sp2;
+		while (1)
 		{
-			return (-1);
+			if (*p1++ != *p2++)
+				return (*--p1 - *--p2);
 		}
-		else if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		i++;
+		while (--n != 0)
+			;
 	}
 	return (0);
 }
--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,32 @@
 /*   By: aboehm <aboehm@42adel.org>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 13:27:58 by aboehm            #+#    #+#             */
-/*   Updated: 2021/09/08 13:29:22 by aboehm           ###   ########.fr       */
+/*   Updated: 2021/09/11 21:05:31 by aboehm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strdup(const char *s)
+{
+	char	*clone;
+	size_t	size;
 
-
+	size = strlen(s) + 1;
+	clone = (char *)malloc(size);
+	if (!clone)
+		return (NULL);
+	memcpy(clone, s, size);
+	return (clone);
+}
+/*
+#include <stdio.h>
+int main ()
+{
+	char thing[] = "this is totally a string!";
+	char *output;
+	output = (char *)ft_strdup((const char *)thing);
+	printf("%s",output);
+	return 0;
+}
+*/
